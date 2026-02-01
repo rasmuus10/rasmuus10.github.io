5bac1d8d6a5a1463e44ba46664c21b2e8b968fd1
rasmuus10 <rasmuslarvia@proton.me>
2026-02-01 20:55:38 +0100

    minor update to park2

--- park2/index.js
+++ park2/index.js
@@ -21,6 +21,21 @@ function render() {
     ctx.fillRect(player.x, player.y, player.width, player.height);
 }
 
+let i = 0;
+
+let plant = new Object(Math.floor(Math.random() * canvas.width), Math.floor(Math.random() * canvas.height), 25, 25);
+
+const drawPlant = setInterval(function() {
+    i++;
+    if(i == 100) { clearInterval(drawPlant); }
+    plant = new Object(Math.floor(Math.random() * canvas.width), Math.floor(Math.random() * canvas.height), 25, 25);
+    ctx.fillStyle = "green";
+    ctx.fillRect(plant.x, plant.y, plant.width, plant.height);
+}, 1);
+
+function isCollidingX(x1, w1, x2, w2) { return x1 < x2 + w2 && x1 + w1 > x2; }
+function isCollidingY(y1, h1, y2, h2) { return y1 < y2 + h2 && y1 + h1 > y2; }
+
 const keys = {};
 
 document.addEventListener("keydown", (event) => {
@@ -40,9 +55,16 @@ function main() {
     if(keys["d"]) { player.x += speed; }
 
     if(player.x < 0) { player.x += speed; }
-    if(player.x > canvas.width) { player.x += speed; }
+    if(player.x > canvas.width - player.width) { player.x -= speed; }
     if(player.y < 0) { player.y += speed; }
-    if(player.x > canvas.height) { player.y -= speed; }
+    if(player.y > canvas.height - player.height) { player.y -= speed; }
+
+    if(
+        isCollidingX(player.x, player.width, plant.x, plant.width) &&
+        isCollidingY(player.y, player.height, plant.y, plant.height)
+    ) {
+        alert("test");
+    }
 
     render();
     requestAnimationFrame(main);
