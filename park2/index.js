8aad939c8e4f5cd5ffba7e92d1e4b4a4fdcc8604
rasmuus10 <rasmuslarvia@proton.me>
2026-02-13 23:59:16 +0100

    changed park2 to entire new thing not a game tho

--- park2/index.js
+++ park2/index.js
@@ -13,27 +13,30 @@ class Object {
     }
 }
 
-const player = new Object(10, 10, 50, 50);
 const speed = 12;
+
+const player = new Object(10, 10, 50, 50);
+
+const wall1 = new Object(400, 0, 50, 500);
+const wall2 = new Object(400, canvas.height - 200, 50, 200);
+const wall3 = new Object(600, 450, 500, 50);
+const wall4 = new Object(1100, 150, 50, 350);
+const wall5 = new Object(1100, 150, 100, 50);
+const wall6 = new Object(canvas.width - 400, 0, 50, 200);
+const wall7 = new Object(canvas.width - 450, 150, 100, 50);
 
 function render() {
     ctx.fillStyle = "blue";
     ctx.fillRect(player.x, player.y, player.width, player.height);
-    ctx.font = "50px Arial";
-ctx.fillText("find the chosen block",100,80);
+    ctx.fillStyle = "silver";
+    ctx.fillRect(wall1.x, wall1.y, wall1.width, wall1.height);
+    ctx.fillRect(wall2.x, wall2.y, wall2.width, wall2.height);
+    ctx.fillRect(wall3.x, wall3.y, wall3.width, wall3.height);
+    ctx.fillRect(wall4.x, wall4.y, wall4.width, wall4.height);
+    ctx.fillRect(wall5.x, wall5.y, wall5.width, wall5.height);
+    ctx.fillRect(wall6.x, wall6.y, wall6.width, wall6.height);
+    ctx.fillRect(wall7.x, wall7.y, wall7.width, wall7.height);
 }
-
-let i = 0;
-
-let plant = new Object(Math.floor(Math.random() * canvas.width), Math.floor(Math.random() * canvas.height), 25, 25);
-
-const drawPlant = setInterval(function() {
-    i++;
-    if(i == 100) { clearInterval(drawPlant); }
-    plant = new Object(Math.floor(Math.random() * canvas.width) - 100, Math.floor(Math.random() * canvas.height), 25, 25);
-    ctx.fillStyle = "green";
-    ctx.fillRect(plant.x, plant.y, plant.width, plant.height);
-}, 1);
 
 function isCollidingX(x1, w1, x2, w2) { return x1 < x2 + w2 && x1 + w1 > x2; }
 function isCollidingY(y1, h1, y2, h2) { return y1 < y2 + h2 && y1 + h1 > y2; }
@@ -51,6 +54,9 @@ document.addEventListener("keyup", (event) => {
 function main() {
     ctx.clearRect(player.x, player.y, player.width, player.height);
 
+    let oldX = player.x;
+    let oldY = player.y;
+
     if(keys["w"]) { player.y -= speed; }
     if(keys["s"]) { player.y += speed; }
     if(keys["a"]) { player.x -= speed; }
@@ -62,11 +68,59 @@ function main() {
     if(player.y > canvas.height - player.height) { player.y -= speed; }
 
     if(
-        isCollidingX(player.x, player.width, plant.x, plant.width) &&
-        isCollidingY(player.y, player.height, plant.y, plant.height)
+        isCollidingX(player.x, player.width, wall1.x, wall1.width) &&
+        isCollidingY(player.y, player.height, wall1.y, wall1.height)
     ) {
-        alert("YOU WON!!!!!");
-        location.reload;
+        player.x = oldX;
+        player.y = oldY;
+    }
+
+    if(
+        isCollidingX(player.x, player.width, wall2.x, wall2.width) &&
+        isCollidingY(player.y, player.height, wall2.y, wall2.height)
+    ) {
+        player.x = oldX;
+        player.y = oldY;
+    }
+
+    if(
+        isCollidingX(player.x, player.width, wall3.x, wall3.width) &&
+        isCollidingY(player.y, player.height, wall3.y, wall3.height)
+    ) {
+        player.x = oldX;
+        player.y = oldY;
+    }
+
+    if(
+        isCollidingX(player.x, player.width, wall4.x, wall4.width) &&
+        isCollidingY(player.y, player.height, wall4.y, wall4.height)
+    ) {
+        player.x = oldX;
+        player.y = oldY;
+    }
+
+    if(
+        isCollidingX(player.x, player.width, wall5.x, wall5.width) &&
+        isCollidingY(player.y, player.height, wall5.y, wall5.height)
+    ) {
+        player.x = oldX;
+        player.y = oldY;
+    }
+
+    if(
+        isCollidingX(player.x, player.width, wall6.x, wall6.width) &&
+        isCollidingY(player.y, player.height, wall6.y, wall6.height)
+    ) {
+        player.x = oldX;
+        player.y = oldY;
+    }
+
+    if(
+        isCollidingX(player.x, player.width, wall7.x, wall7.width) &&
+        isCollidingY(player.y, player.height, wall7.y, wall7.height)
+    ) {
+        player.x = oldX;
+        player.y = oldY;
     }
 
     render();
